5900b53c14d8e77ecccc2009025f61c9ec00b04e
Elshadai Hailu <elshadaihailu@Elshadais-iMac.local>
2023-06-02 14:21:47 -0700

    add logic for the conditions written out in plain english

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,30 @@ window.onlanguagechange = function(){
         rDiv.setAttribute("class", "hidden");
     }
 
+    //logic
+    //if pythonDiv
+    //     Python conditions
+    //     some programming experience
+    //     pursue a career in tech
+    //     yellow
+    //     10 + hours
+    //     18-25
+
+    //else if javaScriptDiv
+    //javaScript conditions
+    //      no programming experience
+    //      pursue a design careers
+    //      green
+    //      5 - 10 hours
+    //      26-35
+    
+
+    //else if rDiv
+    //r conditions
+    //      some programming experience
+    //      enhance research skills
+    //      blue
+    //      35-45
+
+    //else
 }
