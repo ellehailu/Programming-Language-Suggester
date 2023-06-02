e02d101985f5f9c1144b8eb1f98ff1f50d113173
Elshadai Hailu <elshadaihailu@Elshadais-iMac.local>
2023-06-02 11:44:34 -0700

    add new variables to represent each programming language

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 window.onlanguagechange = function(){
     const form = document.querySelector("form");
+    form.onsubmit = function (event) {
+        event.preventDefault();
+    
+        let pythonDiv = document.getElementById("python");
+        pythonDiv.setAttribute("class", "hidden");
+
+        let javaScriptDiv = document.getElementById("javaScript");
+        javaScriptDiv.setAttribute("class", "hidden");
+
+        let rDiv = document.getElementById("r");
+        rDiv.setAttribute("class", "hidden");
+    }
+
 }
